38a1f4a883b271a1ada51c903caa157cbce61f0c
Charlie Jackson <charlie@charliejackson.com>
2017-10-09 13:52:30 +0100

    Split out repeated code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-/* @flow */
 /* eslint no-console: 0 */
 import { spawn } from 'child_process';
+
+/**
+ * Handle the logging functions from the spawn command, also running the
+ * callback function is provided
+ *
+ * @param  {Object} out          The loggin callback from the spawn process
+ * @param  {Function} dataCallback Optional callback to run when something is
+ * logged to the console
+ * @return {Void}              No return value
+ */
+export function log(out, dataCallback) {
+  out.on('data', (data) => {
+    console.log(`${data}`);
+    if (dataCallback) dataCallback(`${data}`);
+  });
+}
 
 /**
  * Spawn a shell command, wrapped in a promise. Optional callback to hook into
@@ -12,20 +27,13 @@ import { spawn } from 'child_process';
  * @return {Promise}              Promise that resolves if the process exits
  * without an error code. Rejects if an exist code is given
  */
-export default function (command: string, dataCallback?: () => {}) {
+export default function (command, dataCallback) {
   return new Promise((resolve, reject) => {
     const commands = command.split(' ');
     const ls = spawn(commands.splice(0, 1)[0], commands);
 
-    ls.stdout.on('data', (data) => {
-      console.log(`${data}`);
-      if (dataCallback) dataCallback(`${data}`);
-    });
-
-    ls.stderr.on('data', (data) => {
-      console.log(`${data}`);
-      if (dataCallback) dataCallback(`${data}`);
-    });
+    log(ls.stdout, dataCallback);
+    log(ls.stderr, dataCallback);
 
     ls.on('close', (code) => {
       if (code) {
